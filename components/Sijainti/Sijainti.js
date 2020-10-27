0f19692941f14ab084507227e0a8c392a70a9751
Ode42 <otso.kurkela@ode-42.com>
2020-10-27 20:20:20 +0200

    add few components and router

---
+++ components/Sijainti/Sijainti.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+function Sijainti({ history }) {
+    return (
+        <View>
+            <Text>Sijainti sivu</Text>
+            <Button title="change page" onPress={() => history.push("/")} />
+        </View>
+    );
+}
+
+export default Sijainti;
